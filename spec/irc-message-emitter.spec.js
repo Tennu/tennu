95b049c28d30d4d326f256d3474a80df10171553
Ryan Scheel <ryan.havvy@gmail.com>
2013-03-02 18:00:41 -0800

    Added a passing test for the irc-message emitter.
    Only needs one. :)

--- spec/irc-message-emitter.spec.js
+++ spec/irc-message-emitter.spec.js
@@ -1,6 +1,7 @@
 var events = require('events');
 
 var MessageHandler = require('../lib/irc-message-emitter');
+var Message = require('../lib/structures/message');
 
 describe('Message Handlers', function () {
     var emitter, mh, receiver;
@@ -11,7 +12,26 @@ describe('Message Handlers', function () {
         mh = new MessageHandler(emitter, {}, receiver);
     });
 
-    it('does something', function () {
-        expect(true).toBe(true);
+    it('convert IRC to Messages', function () {
+        var msg, done = false;
+
+        runs(function () {
+            mh.on('432', function (m) {
+                done = true;
+                msg = m;
+            });
+
+            emitter.emit('data', ':concrete.mozilla.org 432 Havvy[telnet] :Erroneous Nickname: Illegal characters');
+        });
+
+        waitsFor(function () { return done; }, "432 message is parsed.");
+
+        runs(function () {
+            expect(msg instanceof Message).toBeTruthy();
+            expect(msg.name).toBe('432');
+            expect(msg.args[0]).toBe('Havvy[telnet]');
+            expect(msg.args[1]).toBe('Erroneous Nickname: Illegal characters');
+            expect(msg.receiver).toBe(receiver);
+        });
     });
 });
