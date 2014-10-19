35a3e36ec325f8fb80b7f5c45c7e4f10a235ecf1
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2014-10-19 03:51:36 +0200

    Sourcemap support for Mocha testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,36 @@
 var gulp = require('gulp');
 var sweetjs = require("gulp-sweetjs");
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function() {
   console.log("Use either the 'build' or 'test' tasks");
 });
 
-gulp.task("test", function () {
-    gulp.src("src/test/**/*.sjs")
-    .pipe(sweetjs({
-        modules: ['sweet-bdd'],
-        readableNames: true
-    }))
-    .pipe(gulp.dest('test'));
-});
-
 gulp.task("build", function () {
     gulp.src("src/lib/**/*.js")
-    //.pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())
     .pipe(sweetjs({
         modules: [],
         readableNames: true
     }))
-    //.pipe(sourcemaps.write('.'))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('lib'));
 
     gulp.src("src/plugin/**/*.sjs")
-    //.pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())
     .pipe(sweetjs({
         modules: ['sparkler/macros'],
         readableNames: true
     }))
-    //.pipe(sourcemaps.write('.'))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('tennu_plugins'));
 
     gulp.src("src/test/**/*.sjs")
-    //.pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())
     .pipe(sweetjs({
         modules: ['sweet-bdd'],
         readableNames: true
     }))
-    //.pipe(sourcemaps.write('.'))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('test'));
 });
