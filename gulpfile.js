89e80223423fdad534f0386d0aae5979c7c81d82
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2014-10-21 09:26:05 +0200

    No sourcemaps until gulp-sweetjs is fixed.

--- gulpfile.js
+++ gulpfile.js
@@ -8,29 +8,29 @@ gulp.task('default', function() {
 
 gulp.task("build", function () {
     gulp.src("src/lib/**/*.js")
-    .pipe(sourcemaps.init())
+    // .pipe(sourcemaps.init())
     .pipe(sweetjs({
         modules: [],
         readableNames: true
     }))
-    .pipe(sourcemaps.write())
+    // .pipe(sourcemaps.write('../sourcemaps/lib'))
     .pipe(gulp.dest('lib'));
 
     gulp.src("src/plugin/**/*.sjs")
-    .pipe(sourcemaps.init())
+    // .pipe(sourcemaps.init())
     .pipe(sweetjs({
         modules: ['sparkler/macros'],
         readableNames: true
     }))
-    .pipe(sourcemaps.write())
+    // .pipe(sourcemaps.write('../sourcemaps/plugins'))
     .pipe(gulp.dest('tennu_plugins'));
 
     gulp.src("src/test/**/*.sjs")
-    .pipe(sourcemaps.init())
+    // .pipe(sourcemaps.init())
     .pipe(sweetjs({
         modules: ['sweet-bdd'],
         readableNames: true
     }))
-    .pipe(sourcemaps.write())
+    // .pipe(sourcemaps.write('../sourcemaps/test'))
     .pipe(gulp.dest('test'));
 });
