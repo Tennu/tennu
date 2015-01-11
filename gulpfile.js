0e407d88c0c17c9cd78dabdf19dae6008c85ac07
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2015-01-11 18:46:59 +0100

    Add lambda-chop to macro-list, generate sourcemaps.

--- gulpfile.js
+++ gulpfile.js
@@ -7,30 +7,33 @@ gulp.task('default', function() {
 });
 
 gulp.task("build", function () {
+    // lib
     gulp.src("src/lib/**/*.js")
-    // .pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())
     .pipe(sweetjs({
         modules: [],
         readableNames: true
     }))
-    // .pipe(sourcemaps.write('../sourcemaps/lib'))
+    .pipe(sourcemaps.write('../sourcemaps/lib'))
     .pipe(gulp.dest('lib'));
 
+    // plugins
     gulp.src("src/plugin/**/*.sjs")
-    // .pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())
     .pipe(sweetjs({
-        modules: ['sparkler/macros'],
+        modules: ['sparkler/macros', 'lambda-chop/macros'],
         readableNames: true
     }))
-    // .pipe(sourcemaps.write('../sourcemaps/plugins'))
+    .pipe(sourcemaps.write('../sourcemaps/plugins'))
     .pipe(gulp.dest('tennu_plugins'));
 
+    // tests
     gulp.src("src/test/**/*.sjs")
-    // .pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())
     .pipe(sweetjs({
         modules: ['sweet-bdd'],
         readableNames: true
     }))
-    // .pipe(sourcemaps.write('../sourcemaps/test'))
+    .pipe(sourcemaps.write('../sourcemaps/test'))
     .pipe(gulp.dest('test'));
 });
